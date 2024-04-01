cff686d79da4e0a8f33cadef504ecc996576ea93
安小歪 <119773750+shangskr@users.noreply.github.com>
2024-04-01 10:28:31 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-appName="synctv"
+appName="memos"
 builtAt="$(date +'%F %T %z')"
 goVersion=$(go version | awk '{print $3}')
-gitAuthor="SyncTV <pyh1670605849@gmail.com>"
+gitAuthor="Your Name <your_email@example.com>"
 gitCommit=$(git log --pretty=format:"%h" -1)
 version=$(git describe --long --tags --dirty --always)
-webVersion=$(wget -qO- -t1 -T2 "https://api.github.com/repos/synctv-org/synctv-web/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
+webVersion=$(wget -qO- -t1 -T2 "https://api.github.com/repos/usememos/memos/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g')
 
 ldflags="\
 -w -s \
--X 'github.com/synctv-org/synctv/internal/conf.BuiltAt=$builtAt' \
--X 'github.com/synctv-org/synctv/internal/conf.GoVersion=$goVersion' \
--X 'github.com/synctv-org/synctv/internal/conf.GitAuthor=$gitAuthor' \
--X 'github.com/synctv-org/synctv/internal/conf.GitCommit=$gitCommit' \
--X 'github.com/synctv-org/synctv/internal/conf.Version=$version' \
--X 'github.com/synctv-org/synctv/internal/conf.WebVersion=$webVersion' \
+-X 'github.com/usememos/memos/internal/conf.BuiltAt=$builtAt' \
+-X 'github.com/usememos/memos/internal/conf.GoVersion=$goVersion' \
+-X 'github.com/usememos/memos/internal/conf.GitAuthor=$gitAuthor' \
+-X 'github.com/usememos/memos/internal/conf.GitCommit=$gitCommit' \
+-X 'github.com/usememos/memos/internal/conf.Version=$version' \
+-X 'github.com/usememos/memos/internal/conf.WebVersion=$webVersion' \
 "
 
 go build -ldflags="$ldflags" -tags=jsoniter .
